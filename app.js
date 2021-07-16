c824c9717f237f2a4cf94ef1587a8fd80036c857
fraanfx <fran.am95@gmail.com>
2021-07-16 14:57:18 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const protecting = new WeakMap()
+
+class Articulo {
+    constructor(nombre, precio, year){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.year = year;
+    }
+
+}
+
+alert('hola');
+let pName = document.getElementById('pName');
+let pPrice = document.getElementById('pPrice');
+let pYear = document.getElementById('pYear');
+
+
+new Articulo
+function submits (){
+    const articulo = new Articulo(pName,pPrice, pYear);
+    alert('hohhohoho')
+}
